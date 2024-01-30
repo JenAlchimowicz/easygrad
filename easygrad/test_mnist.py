f5232ab27cf9f3f2c7e9bf85b034ab54d6aaba2e
JenAlchimowicz <jedrzejalchimowicz@gmail.com>
2024-01-30 17:11:52 +0100

    Test mnist working & formatted

--- easygrad/test_mnist.py
+++ easygrad/test_mnist.py
@@ -22,13 +22,13 @@ def fetch(url, mnist_path = Path("mnist")):
             f.write(data)
     return data
 
-# def get_mnist():
-#     parse = lambda dat: np.frombuffer(gzip.decompress(dat), dtype=np.uint8).copy()
-#     X_train = parse(fetch("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"))[0x10:].reshape((-1, 28, 28))
-#     Y_train = parse(fetch("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"))[8:]
-#     X_test = parse(fetch("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"))[0x10:].reshape((-1, 28, 28))
-#     Y_test = parse(fetch("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"))[8:]
-#     return X_train, Y_train, X_test, Y_test
+def get_mnist():
+    parse = lambda dat: np.frombuffer(gzip.decompress(dat), dtype=np.uint8).copy()
+    train_images = parse(fetch("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"))[0x10:].reshape((-1, 28*28))
+    train_labels = parse(fetch("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"))[8:]
+    test_images = parse(fetch("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"))[0x10:].reshape((-1, 28*28))
+    test_labels = parse(fetch("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"))[8:]
+    return train_images, train_labels, test_images, test_labels
 
 def layer_init_uniform(*shapes):
     ret = np.random.uniform(-1., 1., size=shapes) / np.sqrt(np.prod(shapes))
@@ -54,49 +54,45 @@ def eval(model):
     return accuracy
 
 
-# Get data
-parse = lambda dat: np.frombuffer(gzip.decompress(dat), dtype=np.uint8).copy()
-train_images = parse(fetch("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"))[0x10:].reshape((-1, 28*28))
-train_labels = parse(fetch("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"))[8:]
-test_images = parse(fetch("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"))[0x10:].reshape((-1, 28*28))
-test_labels = parse(fetch("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"))[8:]
+train_images, train_labels, test_images, test_labels = get_mnist()
 
-# Get model
-model = TensorModel()
+def test_mnist():
+    model = TensorModel()
 
-# Train loop
-batch_size = 128
-steps = 500
-learning_rate = 0.001
+    batch_size = 128
+    steps = 500
+    learning_rate = 0.001
 
-for step in range(steps):
-    sample = np.random.randint(0, train_images.shape[0], size=batch_size)
-    x_train = Tensor(train_images[sample])
-    y_sample = train_labels[sample]
-    y_train = np.zeros((batch_size, 10), dtype=np.float32)
-    y_train[range(y_train.shape[0]), y_sample] = -10.0  # -10 because we have 10 classes
-    y_train = Tensor(y_train)
+    for step in range(steps):
+        sample = np.random.randint(0, train_images.shape[0], size=batch_size)
+        x_train = Tensor(train_images[sample])
+        y_sample = train_labels[sample]
+        y_train = np.zeros((batch_size, 10), dtype=np.float32)
+        y_train[range(y_train.shape[0]), y_sample] = -10.0  # -10 because we have 10 classes
+        y_train = Tensor(y_train)
 
-    # Forward
-    log_probs = model(x_train)
+        # Forward
+        log_probs = model(x_train)
 
-    # NLL loss
-    nll = log_probs.mul(y_train).mean()
+        # NLL loss
+        nll = log_probs.mul(y_train).mean()
 
-    # Backward
-    for param in model.params():
-        param.grad = None
-    nll.backward()
+        # Backward
+        for param in model.params():
+            param.grad = None
+        nll.backward()
 
-    # Update
-    for param in model.params():
-        param.data -= learning_rate * param.grad
+        # Update
+        for param in model.params():
+            param.data -= learning_rate * param.grad
 
-    # Test
-    if step % 20 == 0:
-        preds = np.argmax(log_probs.data, axis=1)
-        accuracy = np.equal(preds, y_sample).sum() / preds.size
-        test_accuracy = eval(model)
-        print(f"Step: {step:03d}, loss: {nll.data.item():.5f}, accuracy: {accuracy:.4f}, test accuracy: {test_accuracy:.4f}")
-
-
+        # Test
+        # if step % 20 == 0:
+        #     preds = np.argmax(log_probs.data, axis=1)
+        #     accuracy = np.equal(preds, y_sample).sum() / preds.size
+        #     test_accuracy = eval(model)
+        #     print(f"Step: {step:03d}, loss: {nll.data.item():.5f}, accuracy: {accuracy:.4f}, test accuracy: {test_accuracy:.4f}")
+    
+    preds = np.argmax(log_probs.data, axis=1)
+    test_accuracy = eval(model)
+    assert test_accuracy > 0.94
