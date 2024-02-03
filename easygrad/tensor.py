6aaa9acf778979ce5965e6185a18fe85c292fcc8
Jen Alchimowicz <jedrzejalchimowicz@gmail.com>
2024-02-04 00:25:46 +0100

    Change backward to topological sort

--- easygrad/tensor.py
+++ easygrad/tensor.py
@@ -20,27 +20,52 @@ class Tensor:
         self.grad = None
         self.ctx = None
 
-    def backward(self, allow_fill=True):
-        if self.ctx is None:  # Nothing to backpropagate to
-            return
-        
-        if self.grad is None and allow_fill:
-            self.grad = np.array([1])
-        
-        grads = self.ctx.op.backward(self.ctx, self.grad)
-        if len(self.ctx.children) == 1:
-            grads = [grads]
-        for child, grad in zip(self.ctx.children, grads):
-            assert(child.data.shape == grad.shape), f"Wrong shapes of gradients, data shape: {child.data.shape}, grad shape: {grad.shape}, op: {self.ctx.op}"
-            child.grad = grad
-            child.backward(False)
+    def topological_sort(self):
+        topo = []
+        visited = set()
+        def build_topo(node):
+            if node not in visited:
+                visited.add(node)
+                if getattr(node, "ctx", False):
+                    for child in node.ctx.children:
+                        build_topo(child)
+                    topo.append(node)
+        build_topo(self)
+        return topo
+
+    def backward(self):
+        assert self.data.size == 1, f"Can only backpropagate on arrays of size 1, got array shape {self.data.shape} and size {self.data.size}"
+
+        self.grad = np.array([1])
+
+        for node in reversed(self.topological_sort()):
+            grads = node.ctx.op.backward(node.ctx, node.grad)
+            if len(node.ctx.children) == 1:
+                grads = [grads]
+            for child, grad in zip(node.ctx.children, grads):
+                assert(child.data.shape == grad.shape), f"Wrong shapes of gradients, data shape: {child.data.shape}, grad shape: {grad.shape}, op: {node.ctx.op}"
+                child.grad = grad if child.grad is None else child.grad + grad
+
+    # def backward(self, allow_fill=True):
+    #     if self.ctx is None:  # Nothing to backpropagate to
+    #         return
+
+    #     if self.grad is None and allow_fill:
+    #         self.grad = np.array([1])
+
+    #     grads = self.ctx.op.backward(self.ctx, self.grad)
+    #     if len(self.ctx.children) == 1:
+    #         grads = [grads]
+    #     for child, grad in zip(self.ctx.children, grads):
+    #         assert(child.data.shape == grad.shape), f"Wrong shapes of gradients, data shape: {child.data.shape}, grad shape: {grad.shape}, op: {self.ctx.op}"
+    #         child.grad = grad
+    #         child.backward(False)
 
     # Non class ops (just use other functions)
     def mean(self):
         div = Tensor(np.array([1/self.data.size], dtype=self.data.dtype))
         return self.sum().mul(div)
-                       
-                       
+
     def __repr__(self):
         return f"Tensor of shape: {self.data.shape}, grad: {self.grad}, Data: {self.data}"
 
@@ -60,7 +85,7 @@ def register(op, name):
 
 
 ###### SIMPLE OPS #######
-    
+
 class Add(Function):
     @staticmethod
     def forward(ctx: Context, x: np.ndarray, y: np.ndarray):
