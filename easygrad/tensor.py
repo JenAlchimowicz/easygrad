1a2953a611ec774c052f35ee68738c7deb9986b2
Jen Alchimowicz <jedrzejalchimowicz@gmail.com>
2024-02-04 00:32:55 +0100

    Clean up backward

--- easygrad/tensor.py
+++ easygrad/tensor.py
@@ -46,21 +46,6 @@ class Tensor:
                 assert(child.data.shape == grad.shape), f"Wrong shapes of gradients, data shape: {child.data.shape}, grad shape: {grad.shape}, op: {node.ctx.op}"
                 child.grad = grad if child.grad is None else child.grad + grad
 
-    # def backward(self, allow_fill=True):
-    #     if self.ctx is None:  # Nothing to backpropagate to
-    #         return
-
-    #     if self.grad is None and allow_fill:
-    #         self.grad = np.array([1])
-
-    #     grads = self.ctx.op.backward(self.ctx, self.grad)
-    #     if len(self.ctx.children) == 1:
-    #         grads = [grads]
-    #     for child, grad in zip(self.ctx.children, grads):
-    #         assert(child.data.shape == grad.shape), f"Wrong shapes of gradients, data shape: {child.data.shape}, grad shape: {grad.shape}, op: {self.ctx.op}"
-    #         child.grad = grad
-    #         child.backward(False)
-
     # Non class ops (just use other functions)
     def mean(self):
         div = Tensor(np.array([1/self.data.size], dtype=self.data.dtype))
