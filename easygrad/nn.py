7deae1577e54e250526b7cf0fbec2e98d0eb246b
Jen Alchimowicz <jedrzejalchimowicz@gmail.com>
2024-03-28 18:30:32 +0100

    Align var naming to pytorch

--- easygrad/nn.py
+++ easygrad/nn.py
@@ -17,7 +17,7 @@ class Linear:
     
 class Embedding:
     def __init__(self, size: int, embed_dim: int):
-        self.weights = Tensor(xavier_uniform(size, embed_dim))
+        self.weight = Tensor(xavier_uniform(size, embed_dim))
         self.vocab_counter = np.arange(size).reshape(1, 1, size)
 
     def __call__(self, idx: np.ndarray):
@@ -28,8 +28,8 @@ class Embedding:
 class LayerNorm:
     def __init__(self, normalized_shape, eps: float = 1e-5):
         assert(isinstance(normalized_shape, int)), f"Only 1d LayerNorm supported, normalized shape should be an int, got {normalized_shape}"
-        self.gamma = Tensor(np.ones(normalized_shape))
-        self.beta = Tensor(np.zeros(normalized_shape))
+        self.weight = Tensor(np.ones(normalized_shape))
+        self.bias = Tensor(np.zeros(normalized_shape))
         self.eps = eps
 
     def __call__(self, x: Tensor):
@@ -39,7 +39,7 @@ class LayerNorm:
         eps = Tensor(np.ones(var.shape) * self.eps)
         std = (var.add(eps)).sqrt()
         y = (x.sub(mean.expand(shape=x.shape))).div(std.expand(shape=x.shape))
-        out = y.mul(self.gamma.expand(shape=x.shape)).add(self.beta.expand(shape=x.shape))
+        out = y.mul(self.weight.expand(shape=x.shape)).add(self.bias.expand(shape=x.shape))
         return out
 
 class Dropout:
